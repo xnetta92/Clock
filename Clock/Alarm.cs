6097f06aed024abfacb74fd174befff259fc6d85
xnetta92 <palanett92@gmail.com>
2023-10-23 11:03:19 +0200

    AlarmClass

---
+++ Clock/Alarm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clock
+{
+    internal class Alarm
+    {
+        public int alarmHour;
+        public int alarmMinute;
+        public bool isAlarmOn;
+
+        public Alarm(int hour, int minute) 
+        {
+            this.alarmHour = hour;
+            this.alarmMinute = minute;
+        }
+
+        public void SetAlarm(int hour, int minute)
+        {
+            this.alarmHour = hour;
+            this.alarmMinute = minute;
+        }
+
+        public void TurnOnAlarm() 
+        {
+            this.isAlarmOn = true; 
+        }
+
+        public void TurnOffAlarm()
+        {
+            this.isAlarmOn = false;
+        }
+
+        public bool IsAlarmTime(int hour, int minute)
+        {
+            if (this.alarmHour == hour && this.alarmMinute == minute) 
+            { 
+                return true; 
+            } else 
+            {
+                return false; 
+            }
+        }
+    }
+}
--- Clock/Program.cs
+++ Clock/Program.cs
@@ -13,6 +13,17 @@ namespace Clock
             var clock = new Clock(12, 4, 37, "UTC+1", true);
             clock.DisplayTime();
 
+            var alarm1 = new Alarm(13, 0);
+            var alarm2 = new Alarm(14, 15);
+            var alarm3 = new Alarm(14, 33);
+
+            alarm1.TurnOnAlarm();
+            alarm2.TurnOffAlarm();
+            alarm3.SetAlarm(14, 30);
+
+            Console.WriteLine(alarm1.IsAlarmTime(12, 58));
+            Console.WriteLine(alarm1.IsAlarmTime(13, 0));
+
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
 
